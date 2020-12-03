a74dd6617c0aabb1eebfeac0d9382aad64c11759
Bertalan Körmendy <korber314@gmail.com>
2020-12-03 10:12:17 +0100

    day 3 complete

--- day3/main.cpp
+++ day3/main.cpp
@@ -2,8 +2,41 @@
 // Created by Berci on 2020. 12. 02..
 //
 
-#include "utils.h";
+#include <iostream>
+#include "utils.h"
+
+int traverse(const std::vector<std::string>& map, size_t step_right, size_t step_down) {
+    const char tree = '#';
+
+    size_t offset = 0;
+    size_t trees = 0;
+
+    for (size_t i = 0; i < map.size(); i += step_down) {
+        const auto& row = map.at(i);
+        if (row[(offset * step_right) % row.size()] == tree) {
+            trees = trees + 1;
+        }
+        offset = offset + 1;
+    }
+
+    return trees;
+}
 
 int main() {
+    auto path = std::filesystem::path("../input/day3.txt");
+    auto lines = lines_from_file(path);
+
+    auto trees = traverse(lines, 3, 1);
+
+    std::cout << "Part one: " << trees << std::endl;
+
+    auto total_trees
+        = traverse(lines, 1, 1)
+        * traverse(lines, 3, 1)
+        * traverse(lines, 5, 1)
+        * traverse(lines, 7, 1)
+        * traverse(lines, 1, 2);
+
+    std::cout << "Part two: " << total_trees << std::endl;
 
 }
