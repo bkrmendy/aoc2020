f90bfd52d01008afd5cb9575bdcaba8d9bcdc4f7
Bertalan Körmendy <korber314@gmail.com>
2020-12-07 08:24:44 +0100

    prepare day 8

---
+++ day8/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Berci on 2020. 12. 07..
+//
+
+#include "utils.h"
+
+int main() {
+    auto path = std::filesystem::path("../input/day7.txt");
+    auto lines = lines_from_file(path);
+}
