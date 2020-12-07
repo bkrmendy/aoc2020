aaca241bdfb323609fe8864d7f15a3ff14da7f9e
Bertalan Körmendy <korber314@gmail.com>
2020-12-07 08:22:23 +0100

    day7 complete

--- day7/main.cpp
+++ day7/main.cpp
@@ -3,11 +3,92 @@
 //
 
 #include <filesystem>
+#include <map>
+#include <set>
+#include <iostream>
+#include <utility>
+#include <numeric>
 
 #include "utils.h"
 
+struct Entry {
+    int quantity;
+    std::string color;
+
+    Entry(int quantity, std::string color) : quantity(quantity), color(std::move(color)) {}
+};
+
+bool bag_can_contain(std::string& color,
+                     std::vector<Entry>& contained,
+                     std::map<std::string, std::vector<Entry>>& bags) {
+    for (const auto& entry : contained) {
+        if (entry.color == color) {
+            return true;
+        }
+        if (bag_can_contain(color, bags.at(entry.color), bags)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+int bag_contains_n(std::vector<Entry>& entries, std::map<std::string, std::vector<Entry>>& bags) {
+    return std::reduce(entries.begin(),
+                       entries.end(),
+                       0,
+                       [&bags](int acc, Entry& entry) {
+        int contained = bag_contains_n(bags.at(entry.color), bags);
+       return acc + entry.quantity + entry.quantity * contained;
+    });
+}
+
 int main() {
-    auto path = std::filesystem::path("../input/day1.txt");
+    auto path = std::filesystem::path("../input/day7.txt");
+    auto lines = lines_from_file(path);
+
+    std::map<std::string, std::vector<Entry>> entries{};
+
+    for (const auto& line : lines) {
+        auto parts = split(line, " bags contain ");
+
+        if (parts.at(1).find("no other") != std::string::npos) {
+            entries.emplace(parts.at(0), std::vector<Entry>{});
+        } else {
+            const auto contained = split(parts.at(1).substr(0, parts.at(1).size() - 1), ", ");
+
+            std::vector<Entry> colors_amounts{};
+
+            for (const auto& entry : contained) {
+                int amount = std::stoi(entry.substr(0, 1));
+                std::string color = entry.substr(2, entry.find(" bag") - 2);
+                colors_amounts.emplace_back(amount, color);
+            }
+
+            entries.emplace(parts.at(0), colors_amounts);
+        }
+    }
+
+    std::string target = "shiny gold";
+
+    std::set<std::string> bags;
+    for (auto& [bag, contained] : entries) {
+        if (bag_can_contain(target, contained, entries)) {
+            bags.insert(bag);
+        }
+    }
+
+    std::cout << "Part one: " << bags.size() << std::endl;
+
+    int shiny_contains = 0;
+
+    for (auto& [bag, bags] : entries) {
+        if (bag == target) {
+            shiny_contains += bag_contains_n(bags, entries);
+        }
+    }
+
+    std::cout << "Part two: " << shiny_contains << std::endl;
 
     return 0;
 }
