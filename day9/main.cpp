6a35f031a96a2e8cfceab96088557ef0d1e71284
Bertalan Körmendy <korber314@gmail.com>
2020-12-09 09:06:00 +0100

    day 9 complete

--- day9/main.cpp
+++ day9/main.cpp
@@ -5,14 +5,54 @@
 #include <filesystem>
 
 #include <fmt/core.h>
-#include "util.h"
+#include <set>
+#include "utils.h"
+
+long part_one(std::vector<long>& entries) {
+    size_t preamble_length = 25;
+
+    bool found = false;
+    for (size_t i = preamble_length; i < entries.size(); i++) {
+        for (size_t k = i - preamble_length; k < i; k++) {
+            for (size_t l = i - preamble_length; l < i && l != k; l++) {
+                found |= (entries.at(i) == entries.at(l) + entries.at(k));
+            }
+        }
+        if (!found) {
+            return entries.at(i);
+
+        } else {
+            found = false;
+        }
+    }
+    return -1;
+}
 
 int main() {
     auto path = std::filesystem::path("../input/day9.txt");
 
-    auto part_one = 1;
-    auto part_two = 2;
+    std::vector<long> entries = entries_from_file<long>(path, [](const std::string& line) {
+       return std::stol(line);
+    });
 
-    fmt::print("Part one: {}\n", part_one);
-    fmt::print("Part one: {}\n", part_two);
+    long n = part_one(entries);
+    fmt::print("Part one: {}\n", n);
+
+    for (size_t i = 0; i < entries.size() - 1; i++) {
+        std::set<long> nums{};
+
+        nums.insert(entries.at(i));
+        long acc = entries.at(i);
+
+        for (size_t k = i + 1; k < entries.size() && acc <= n; k++) {
+            if (acc == n && nums.size() > 1) {
+                long min = *nums.begin();
+                long max = *nums.rbegin();
+                fmt::print("Part two: {}\n", min + max);
+            }
+
+            nums.insert(entries.at(k));
+            acc += entries.at(k);
+        }
+    }
 }
