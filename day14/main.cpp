59b89d56f225f573b3907ead385188b732a068dc
Bertalan Körmendy <korber314@gmail.com>
2020-12-13 22:03:26 +0100

    prepare day 14

---
+++ day14/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Berci on 2020. 12. 13..
+//
+
+#include <fmt/core.h>
+#include "utils.h"
+
+int main() {
+    auto path = std::filesystem::path{"../input/day12.txt"};
+
+    fmt::print("Part one: {}\n", 1337);
+    fmt::print("Part two: {}\n", 8888);
+}
