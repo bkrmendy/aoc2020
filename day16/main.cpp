6fa46e3de2f90826d64ba76f88788ce1dec8a04d
Bertalan Körmendy <korber314@gmail.com>
2020-12-15 23:27:17 +0100

    prerare day 16

---
+++ day16/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Berci on 2020. 12. 15..
+//
+
+#include <fmt/core.h>
+#include "utils.h"
+
+int main() {
+    auto path = std::filesystem::path{"../input/day16.txt"};
+
+    fmt::print("Part one: {}\n", 1);
+    fmt::print("Part two: {}\n", 2);
+}
