78a2472e968409941f3a01fd1aefc71f18901202
Bertalan Körmendy <korber314@gmail.com>
2020-12-16 20:13:57 +0100

    day 16 wip

--- aocutils/utils.h
+++ aocutils/utils.h
@@ -6,6 +6,7 @@
 #define AOC2020_UTILS_H
 
 #include <vector>
+#include <set>
 #include <string>
 #include <filesystem>
 #include <functional>
@@ -21,6 +22,28 @@ std::vector<T> entries_from_file(std::filesystem::path& path, std::function<T(co
     std::transform(lines.begin(), lines.end(), std::back_inserter(entries), make_entry);
 
     return entries;
+}
+
+template<typename T>
+std::set<T> set_intersection(std::vector<std::set<T>>& sets) {
+    if (sets.empty()) {
+        return std::set<T>{};
+    }
+
+    if (sets.size() == 1) {
+        return sets.at(0);
+    };
+
+    std::set<T> intersect = sets.at(0);
+    for (const auto& set : sets) {
+        std::set<T> intersect2{};
+        std::set_intersection(set.begin(), set.end(),
+                              intersect.begin(), intersect.end(),
+                              std::inserter(intersect2, intersect2.begin()));
+        intersect = intersect2;
+    }
+
+    return intersect;
 }
 
 std::vector<std::string> split(const std::string& str, const std::string& delimiter);
--- day16/main.cpp
+++ day16/main.cpp
@@ -2,15 +2,118 @@
 // Created by Berci on 2020. 12. 15..
 //
 
-#include
+#include <string>
+#include <numeric>
+#include <set>
+#include <map>
+
+#include <ctre.h>
 #include <fmt/core.h>
+
 #include "utils.h"
 
+class Constraint {
+    int u1;
+    int u2;
+    int h1;
+    int h2;
+public:
+    Constraint(int u1, int u2, int h1, int h2) : u1(u1), u2(u2), h1(h1), h2(h2) {}
+    bool matches(const int number) const {
+        return (u1 <= number && number <= u2)
+                || (h1 <= number && number <= h2);
+    }
+};
+
+std::string your_ticket = "83,137,101,73,67,61,103,131,151,127,113,107,109,89,71,139,167,97,59,53";
+
+constexpr auto pattern = ctll::fixed_string{"([a-z: ]*)([0-9]+)-([0-9]+) or ([0-9]+)-([0-9]+)"};
+
+std::vector<Constraint> read_constrains(std::vector<std::string>&& lines) {
+    std::vector<Constraint> constraints;
+    for (const auto& line : lines) {
+        if (auto m = ctre::match<pattern>(line)) {
+            auto u1 = std::stoi(m.get<2>().to_string());
+            auto u2 = std::stoi(m.get<3>().to_string());
+            auto h1 = std::stoi(m.get<4>().to_string());
+            auto h2 = std::stoi(m.get<5>().to_string());
+            constraints.emplace_back(u1, u2, h1, h2);
+        }
+    }
+
+    return constraints;
+}
+
+std::vector<int> read_ticket(std::string& line) {
+    auto nums = split(line, ",");
+    std::vector<int> vals{};
+    std::transform(nums.begin(), nums.end(), std::back_inserter(vals), [](auto n) { return std::stoi(n); });
+    return vals;
+}
+
+std::vector<std::vector<int>> read_tickets(std::vector<std::string>&& lines) {
+    std::vector<std::vector<int>> tickets{};
+    for (auto& line : lines) {
+        tickets.emplace_back(read_ticket(line));
+    }
+    return tickets;
+}
+
+int part_one(std::filesystem::path& nearby_path, std::filesystem::path& constraints_path) {
+    std::vector<Constraint> constraints = read_constrains(lines_from_file(constraints_path));
+    std::vector<std::vector<int>> tickets = read_tickets(lines_from_file(nearby_path));
+
+    int total = 0;
+    for (size_t i = 0; i < tickets.size(); i++) {
+        for (size_t j = 0; j < tickets.at(i).size(); j++) {
+            auto value = tickets.at(i).at(j);
+            bool ok = std::any_of(constraints.begin(),
+                                  constraints.end(),
+                                  [&value](Constraint c) { return c.matches(value); });
+            if (not ok) {
+                total += value;
+            }
+        }
+    }
+
+    return total;
+}
+
+int part_two(std::filesystem::path& nearby_path, std::filesystem::path& constraints_path) {
+    std::vector<Constraint> constraints = read_constrains(lines_from_file(constraints_path));
+    std::vector<std::vector<int>> tickets = read_tickets(lines_from_file(nearby_path));
+    auto this_ticket = read_ticket(your_ticket);
+
+    std::set<size_t> matchers{};
+    for (size_t c = 0; c < constraints.size(); c++) {
+        matchers.insert(c);
+    }
+
+    std::map<size_t, size_t> constraint_to_pos{};
+
+    for(size_t i = 0; i < this_ticket.size(); i++) {
+        std::set<size_t> these_matchers = matchers;
+        for (size_t j = 0; j < tickets.size(); j++) {
+            std::set<size_t> m{};
+            for (auto c : these_matchers) {
+                if (constraints.at(c).matches(tickets.at(j).at(i))) {
+                    m.insert(c);
+                }
+            }
+            these_matchers = m;
+        }
+        assert(these_matchers.size() == 1);
+        constraint_to_pos.emplace(*these_matchers.begin(), i);
+    }
+
+    auto g = 0;
+
+}
+
 int main() {
-    auto path = std::filesystem::path{"../input/day16.txt"};
+    auto path = std::filesystem::path{"../input/day16_nearby.txt"};
+    auto constraints_path = std::filesystem::path{"../input/day16_constraints.txt"};
 
-    auto pattern =
-
-    fmt::print("Part one: {}\n", 1);
-    fmt::print("Part two: {}\n", 2);
+    fmt::print("Part one: {}\n", part_one(path, constraints_path));
+    fmt::print("Part two: {}\n", part_two(path, constraints_path));
 }
