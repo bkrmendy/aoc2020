12a96c0e553058f93c3c5a80e97eb9c0a84dbeb0
Bertalan Körmendy <korber314@gmail.com>
2020-12-19 00:02:42 +0100

    reset day 18 part 1 to "simple" solution

--- day18/main.cpp
+++ day18/main.cpp
@@ -58,7 +58,7 @@ Step evaluate(std::string& tokens, size_t from, size_t to);
 Step evaluate_expr(std::string& tokens, size_t from) {
     char this_token = tokens.at(from);
     if (is_number(this_token)) {
-        return Step(from + 1, std::make_shared<Number>(to_number(this_token)));
+        return Step(from + 1, to_number(this_token));
     }
 
     auto [to, expr] = evaluate(tokens, from + 1, closing_paren_pos(tokens, from));
@@ -70,51 +70,23 @@ Step evaluate(std::string& tokens, size_t from, size_t to) {
     auto [to_e, expr] = evaluate_expr(tokens, from);
 
     size_t this_pos = to_e;
-    std::shared_ptr<Expression> l_expr = expr;
+    long l_value = expr;
 
     while (this_pos < to) {
         auto op = get_operator(tokens, this_pos);
         auto [to_r, r_expr] = evaluate_expr(tokens, this_pos + 1);
 
-        l_expr = std::make_shared<Compound>(op, l_expr, r_expr);
+        if (op == Operator::Add) {
+            l_value = l_value + r_expr;
+        } else if (op == Operator::Multiply) {
+            l_value = l_value * r_expr;
+        } else {
+            assert(0 && "Unknown operator");
+        }
 
         this_pos = to_r;
     }
-    return Step(this_pos, l_expr);
-}
-
-std::shared_ptr<Expression> balance(const std::shared_ptr<Expression>& e) {
-    if (auto n = std::dynamic_pointer_cast<Number>(e)) {
-        return n;
-    }
-
-    if (auto c = std::dynamic_pointer_cast<Compound>(e)) {
-        if (c->op() == Operator::Add) {
-            if (std::dynamic_pointer_cast<Number>(c->left())
-                && std::dynamic_pointer_cast<Number>(c->right())) {
-                return c;
-            }
-
-            if (auto left = std::dynamic_pointer_cast<Compound>(c->left())) {
-                if (left->op() == Operator::Multiply) {
-                    c = rotate_right(c, left);
-                }
-            }
-
-            if (auto right = std::dynamic_pointer_cast<Compound>(c->right())) {
-                if (right->op() == Operator::Multiply) {
-                    c = rotate_left(c, right);
-                }
-            }
-        }
-
-        return std::make_shared<Compound>(
-                c->op(),
-                balance(c->left()),
-                balance(c->right())
-        );
-    }
-    assert(0 && "Unknown expression");
+    return Step(this_pos, l_value);
 }
 
 long part_one(std::vector<std::string>& lines) {
@@ -124,19 +96,8 @@ long part_one(std::vector<std::string>& lines) {
                        [](auto acc, auto line) {
         auto [to, res] = evaluate(line, 0, line.size());
         assert(to == line.size());
-        return acc + res->evaluate();
+        return acc + res;
     });
-}
-
-long part_two(std::vector<std::string>& lines) {
-    return std::reduce(lines.begin(),
-                       lines.end(),
-                       0l,
-                       [](auto acc, auto line) {
-       auto[to, res] = evaluate(line, 0, line.size());
-       assert(to == line.size());
-       return acc + balance(res)->evaluate();
-   });
 }
 
 int main() {
@@ -145,5 +106,4 @@ int main() {
     assert(lines.empty() == false);
 
     fmt::print("Part one: {}\n", part_one(lines));
-    fmt::print("Part one: {}\n", part_two(lines));
 }
