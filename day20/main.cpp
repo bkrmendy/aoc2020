8a37f3ab3ff0174588e8227030fe03e18a0c5b7a
Bertalan Körmendy <korber314@gmail.com>
2020-12-20 20:50:40 +0100

    prepare day 20

---
+++ day20/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Berci on 2020. 12. 20..
+//
+
+#include <fmt/core.h>
+#include "utils.h"
+
+int main() {
+    fmt::print("Part one: {}\n", 22);
+    fmt::print("Part two: {}\n", 33);
+}
