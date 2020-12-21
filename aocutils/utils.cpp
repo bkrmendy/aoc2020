8ce89d37dcedccea529cd83e09e9ce72127255fb
Bertalan Körmendy <korber314@gmail.com>
2020-12-21 22:37:17 +0100

    fix split to disregard zero length tokens

--- aocutils/utils.cpp
+++ aocutils/utils.cpp
@@ -23,12 +23,16 @@ std::vector<std::string> split(const std::string& str, const std::string& delimi
     std::vector<std::string> res;
 
     while ((pos_end = str.find (delimiter, pos_start)) != std::string::npos) {
-        token = str.substr (pos_start, pos_end - pos_start);
+        token = str.substr(pos_start, pos_end - pos_start);
         pos_start = pos_end + delim_len;
-        res.push_back (token);
+        if (!token.empty()) {
+            res.push_back(token);
+        }
     }
-
-    res.push_back (str.substr(pos_start));
+    token = str.substr(pos_start);
+    if (!token.empty()) {
+        res.push_back(token);
+    }
     return res;
 }
 
