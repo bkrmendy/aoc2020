b6991f03242253ce6e09a61d923a4c95ccd6e56e
Bertalan Körmendy <korber314@gmail.com>
2020-12-27 14:31:56 +0100

    day 19 complete

--- day19/main.cpp
+++ day19/main.cpp
@@ -14,62 +14,53 @@
 #include "overloaded.h"
 #include "utils.h"
 
-using Letter = std::string;
+using Letter = char;
 using Sequence = std::vector<int>;
 using Pipe = std::pair<Sequence, Sequence>;
 using Rule = std::variant<Letter, Pipe, Sequence>;
 
-std::vector<std::string> cartesian_product(std::vector<std::vector<std::string>>& sets) {
-    std::vector<std::string> result{""};
-    for (const auto& result_set : sets) {
-        std::vector<std::string> intermediate_results{};
-        for (const auto& res : result) {
-            for (const auto& entry : result_set) {
-                intermediate_results.push_back(res);
-                intermediate_results.back() += entry;
-            }
-        }
-        result = std::move(intermediate_results);
+using MatchResult = std::pair<std::string::iterator, bool>;
+
+MatchResult matches(std::string::iterator begin,
+                    std::string::iterator end,
+                    std::map<int, Rule>& rules,
+                    Rule& rule) {
+    if (begin == end) {
+        return MatchResult{begin, false};
     }
-    return result;
-}
 
-std::vector<std::string> matches_for(std::map<int, Rule>& rules, Rule& rule) {
     return std::visit(overloaded {
-            [](Letter& letter) {
-                return std::vector<std::string>{letter};
-            },
-            [&rules](Sequence & sequence) {
-                std::vector<std::vector<std::string>> all_matches{};
-                for (auto rule : sequence) {
-                    all_matches.emplace_back(matches_for(rules, rules.at(rule)));
+            [&begin](Letter& letter) -> MatchResult {
+                if (*begin == letter) {
+                    return {std::next(begin), true};
                 }
-                return cartesian_product(all_matches);
+                return {begin, false};
             },
-            [&rules](Pipe& pipe) {
-                std::vector<std::string> all_matches{};
-                auto& [left, right] = pipe;
+            [&begin, &end, &rules](Sequence& seq) -> MatchResult {
+                auto cur = begin;
+                for (auto& elem : seq) {
+                    auto [it, matched] = matches(cur, end, rules, rules.at(elem));
+                    if (not matched) {
+                        return MatchResult{begin, false};
+                    }
+                    cur = it;
+                }
+                return MatchResult{cur, true};
+            },
+            [&begin, &end, &rules](Pipe& pipe) -> MatchResult {
+                auto l_rule = Rule{pipe.first};
+                auto r_rule = Rule{pipe.second};
 
-                auto l_rule = Rule{left};
-                auto r_rule = Rule{right};
-
-                std::vector<std::string> left_matches = matches_for(rules, l_rule);
-                std::vector<std::string> right_matches = matches_for(rules, r_rule);
-
-                all_matches.insert(all_matches.end(),
-                                   std::make_move_iterator(left_matches.begin()),
-                                   std::make_move_iterator(left_matches.end()));
-
-                all_matches.insert(all_matches.end(),
-                                   std::make_move_iterator(right_matches.begin()),
-                                   std::make_move_iterator(right_matches.end()));
-
-                return all_matches;
+                auto [it, matched] = matches(begin, end, rules, l_rule);
+                if (matched) {
+                    return MatchResult{it, true};
+                }
+                return matches(begin, end, rules, r_rule);
             }
     }, rule);
 }
 
-constexpr auto letter_pattern = ctll::fixed_string{"([0-9]+): '(a|b)'"};
+constexpr auto letter_pattern = ctll::fixed_string{R"(([0-9]+): '(a|b)')"};
 constexpr auto sequence_pattern = ctll::fixed_string{R"(([0-9]+): (([0-9]+\s?)+))"};
 constexpr auto pipe_pattern = ctll::fixed_string{R"(([0-9]+): (([0-9]+\s?)+)\| (([0-9]+\s?)+))"};
 
@@ -79,7 +70,7 @@ std::map<int, Rule> read_rules(std::filesystem::path&& path) {
         if (auto letter = ctre::match<letter_pattern>(line)) {
             auto number = letter.get<1>().to_string();
             auto chr = letter.get<2>().to_string();
-            rules.emplace(std::stoi(number), chr);
+            rules.emplace(std::stoi(number), chr[0]);
         } else if (auto seq = ctre::match<sequence_pattern>(line)) {
             auto number = seq.get<1>().to_string();
             std::vector<int> numbers{};
@@ -112,27 +103,76 @@ std::map<int, Rule> read_rules(std::filesystem::path&& path) {
     return rules;
 }
 
-int n_messages_match(std::map<int, Rule>&& rules, std::vector<std::string>& messages) {
-    auto possible_messages = matches_for(rules, rules.at(0));
+/**
+ * solution due to reddit user u/Zv0n in day 19 solution thread
+ */
+int part2(std::map<int, Rule>& rules, std::vector<std::string>& messages) {
+    int total = 0;
 
+    for (auto& message : messages) {
+        auto cur = message.begin();
+        auto end = message.end();
+
+        auto c42 = 0;
+        auto c31 = 0;
+
+        // match 42s
+        while (cur < end) {
+            auto [it, matched] = matches(cur, end, rules, rules.at(42));
+            cur = it;
+            if (matched) {
+                c42 += 1;
+            } else {
+                break;
+            }
+        }
+
+        // match 31s
+        while (cur < end) {
+            auto [it, matched] = matches(cur, end, rules, rules.at(31));
+            cur = it;
+            if (matched) {
+                c31 += 1;
+            } else {
+                break;
+            }
+        }
+
+        if (1 <= c31
+            && 2 <= c42
+            && c31 < c42
+            && cur == end) {
+            total += 1;
+        }
+    }
+
+    return total;
+}
+
+int n_messages_match(std::map<int, Rule>& rules, std::vector<std::string>& messages) {
     return std::count_if(messages.begin(),
                          messages.end(),
-                         [&possible_messages](std::string& message) {
-                              return std::find(possible_messages.begin(),
-                                               possible_messages.end(),
-                                               message) != possible_messages.end();
-                        });
+                         [&rules](std::string& message) {
+                            auto [it, does_match] = matches(message.begin(),
+                                                            message.end(),
+                                                            rules,
+                                                            rules.at(0));
+
+                            bool reached_end = it == message.end();
+
+                            return does_match && reached_end;
+        });
 }
 
 int main() {
-    auto messages_path = std::filesystem::path{"../input/day19_messages.txt"};
+    auto messages_path = std::filesystem::path{"../input/day19/messages.txt"};
     auto messages = lines_from_file(messages_path);
+    auto rules = read_rules(std::filesystem::path{"../input/day19/rules.txt"});
 
-//    fmt::print("Part one: {}\n",
-//               n_messages_match(read_rules(std::filesystem::path{"../input/day19_rules.txt"}),
-//                                messages));
+    fmt::print("Part one: {}\n", n_messages_match(rules, messages));
 
-    fmt::print("Part two: {}\n",
-               n_messages_match(read_rules(std::filesystem::path{"../input/day19_rules_mod.txt"}),
-                                messages));
+    rules.at(8) = Pipe{Sequence{42}, Sequence{42, 8}};
+    rules.at(11) = Pipe{Sequence{42, 31}, Sequence{42, 11, 31}};
+
+    fmt::print("Part two: {}\n", part2(rules, messages));
 }
